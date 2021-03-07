f47c917487914f499892e31802626b6b72f352ba
alone_as_a_god <senoriship@gmail.com>
2021-03-08 00:05:10 +0100

    Added dictionary for prefix

--- discordBot/server.py
+++ discordBot/server.py
@@ -1,14 +1,16 @@
 import socket
 
+
 def notificationListener():                         #method gets called upon bot startup (bot.onready), accepts socket connections and prints received messages
     s = socket.socket()                             #Creates new socket
+    ip = "127.0.0.1"
     port = 12345
-    s.bind(('', port))                              #Binds the ip and port to the socket (127.0.0.1:12345)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((ip, port))                              #Binds the ip and port to the socket (127.0.0.1:12345)
     s.listen(5)                                     #Listens for up to 5 connections
     while True:                                         
         c, addr = s.accept()                        #accepts new connections
         print("Connection from",addr)
         rcvdData = c.recv(1024).decode()            #receives data sent, and prints it as a string
         print ("received:",rcvdData)
-
         c.close()                                   #since only one message is sent at a time, the socket gets closed again, to make sure other connections are possible
