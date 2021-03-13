442a3d7ed87bc1fb97ab4b72d0fe907bcc4caf67
simonnieder <64171900+simonnieder@users.noreply.github.com>
2021-03-14 00:59:06 +0100

    Delete socket-client directory

--- socket-client/index.js
+++
@@ -1,9 +0,0 @@
-import io from "socket.io-client";
-
-let socket = io.connect("http://localhost:4000/");
-
-socket.emit("message", "message");
-
-socket.on("message", (message) => {
-  console.log(message);
-});
