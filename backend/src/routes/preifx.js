24ca82d2a2588cae8b67d6bb13e4506c062d4d0d
simonnieder <niederwolfsgruber16@gmail.com>
2021-03-29 15:29:00 +0200

    get and set prefix, merge conflicts

--- backend/src/routes/preifx.js
+++ backend/src/routes/preifx.js
@@ -13,77 +13,74 @@ router.get("/:guildID", (req, res) => {
         FROM guild
         WHERE guildID = ?`;
 
-    db.get(sql, [req.params.guildID], (err, row) => {
-        if (err) {
-            //TODO: send better error message
-            res.sendStatus(400);
-            return console.error(err.message);
-        } 
+  db.get(sql, [req.params.guildID], (err, row) => {
+    if (err) {
+      //TODO: send better error message
+      res.sendStatus(400);
+      return console.error(err.message);
+    }
 
-        //Check if row contains data
-        if(row){
-            res.json({prefix: row.prefix});
-        }else{
-            res.sendStatus(404);
-        }
-    });
+    //Check if row contains data
+    if (row) {
+      res.json({ prefix: row.prefix });
+    } else {
+      res.sendStatus(404);
+    }
+  });
 
-    db.close();
+  db.close();
 });
 
 //Change the prefix of a specific guild
 router.post("/", (req, res) => {
-    console.log(req.body)
-    let prefix = req.body.prefix;
-    let guildID =  req.body.guildID;
+  console.log(req.body);
+  let prefix = req.body.prefix;
+  let guildID = req.body.guildID;
 
-    //Check if both values are not empty
-    if(!prefix || ! guildID){
-        res.sendStatus(400);
-        return;
-    }
+  //Check if both values are not empty
+  if (!prefix || !guildID) {
+    res.sendStatus(400);
+    return;
+  }
 
-    let db = new sqlite3.Database(dbPath);
-    let sql = `
+  let db = new sqlite3.Database(dbPath);
+  let sql = `
         UPDATE guild
         SET prefix = ?
         WHERE guildID = ?`;
 
-    db.run(sql, [prefix, guildID], (err) => {
-        if (err) {
-            //TODO: send better error message
-            res.sendStatus(400);
-            return console.error(err.message);
-        } 
-        
-        console.log(`Updated prefix of guild: ${guildID} to ${prefix}`);
+  db.run(sql, [prefix, guildID], (err) => {
+    if (err) {
+      //TODO: send better error message
+      res.sendStatus(400);
+      return console.error(err.message);
+    }
 
-        sendNotification("updateDictionary");
-        res.sendStatus(200);     //TODO: Return error code if sendNotification failed
-    });
+    console.log(`Updated prefix of guild: ${guildID} to ${prefix}`);
 
-    db.close();
-});
+    sendNotification("updateDictionary");
+    res.sendStatus(200); //TODO: Return error code if sendNotification failed
+  });
 
   db.close();
 });
 
 //Send a notification to the Pyhton server
 function sendNotification(msg) {
-    const client = new net.Socket();
+  const client = new net.Socket();
 
-    client.on("error", (err) => {
-        console.log(`An error occured: ${err}`);
-    });
+  client.on("error", (err) => {
+    console.log(`An error occured: ${err}`);
+  });
 
-    const botIp = process.env.BOT_ADDRESS || "localhost";
-    const botPort = process.env.BOT_PORT || 12345;
-   
-    client.connect(botPort, botIp, () => {
-        console.log(`Connected to ${botIp}:${botPort}`);
-        client.write(msg);
-        client.destroy();
-    });
+  const botIp = process.env.BOT_ADDRESS || "localhost";
+  const botPort = process.env.BOT_PORT || 12345;
+
+  client.connect(botPort, botIp, () => {
+    console.log(`Connected to ${botIp}:${botPort}`);
+    client.write(msg);
+    client.destroy();
+  });
 }
 
 module.exports = router;
